b8fc4bedcb46aef8fb38dfcbd0662d8da6700117
Oleg764 <oleg_764@mail.ru>
2022-07-21 03:29:13 +0500

    D.z.3

--- Python2/Ex2.4.py
+++ Python2/Ex2.4.py
@@ -1,3 +1,6 @@
+#5.Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. 
+#Позиции вводятся пользователем
+
 N = int(input('Введите число  n : '))
 numbers =[]
 for i in range(-N, N+1):
@@ -8,6 +11,13 @@ print (numbers)
 int_list = []
 for element in input('Введите 2 номера позиций через пробел  : ').split():
     int_list.append(int(element))
+    composition=1
+    composition*=numbers[int_list[element]]
+    
 
-composition_elem =numbers[int_list [0]]*numbers[int_list [1]]
-print ('Произведение элементов = ',composition_elem)
+#for element in range[0:-1]:
+    
+    #composition_elem*=int(numbers[int_list[element]])
+#composition_elem =numbers[int_list [0]]*numbers[int_list [1]]
+#print ('Произведение элементов = ',composition_elem)
+print (composition)
